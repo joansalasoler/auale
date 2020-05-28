17d5697e66b7e7a822219e3d16b839473061ac44
Joan Sala Soler <contact@joansala.com>
2020-05-28 17:46:48 +0200

    Widgets for the main modal dialogs

--- src/auale/gui/widgets/__init__.py
+++ src/auale/gui/widgets/__init__.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from .about_dialog import AboutDialog
 from .match_chooser_dialog import MatchChooserDialog
+from .new_match_dialog import NewMatchDialog
 from .open_match_dialog import OpenMatchDialog
+from .properties_dialog import PropertiesDialog
 from .recent_chooser_menuitem import RecentChooserMenuitem
 from .recent_chooser_popover_menu import RecentChooserPopoverMenu
 from .request_save_dialog import RequestSaveDialog
 from .save_match_dialog import SaveMatchDialog
 
 __all__ = [
+    'AboutDialog',
     'MatchChooserDialog',
+    'NewMatchDialog',
     'OpenMatchDialog',
+    'PropertiesDialog',
     'RecentChooserMenuitem',
     'RecentChooserPopoverMenu',
     'RequestSaveDialog',
