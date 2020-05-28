4a23e1a39f83470f78390f207586532d789a3bdf
Joan Sala Soler <contact@joansala.com>
2020-05-28 16:35:02 +0200

    File filters for the dialogs

---
+++ src/auale/gui/filters/any_file_filter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Aualé oware graphic user interface.
+# Copyright (C) 2014-2020 Joan Sala Soler <contact@joansala.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+
+
+class AnyFileFilter(Gtk.FileFilter):
+    """A filter for Oware Game Notation files"""
+
+    __gtype_name__ = 'AnyFileFilter'
+
+    def __init__(self):
+        super(AnyFileFilter, self).__init__()
+
+        self.set_name(_("Any files"))
+        self.add_pattern('*')
---
+++ src/auale/gui/filters/ogn_file_filter.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Aualé oware graphic user interface.
+# Copyright (C) 2014-2020 Joan Sala Soler <contact@joansala.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+
+
+class OGNFileFilter(Gtk.FileFilter):
+    """A filter for Oware Game Notation files"""
+
+    __gtype_name__ = 'OGNFileFilter'
+
+    def __init__(self):
+        super(OGNFileFilter, self).__init__()
+
+        self.set_name(_("Oware match files"))
+        self.add_mime_type('text/x-oware-ogn')
+        self.add_pattern('*.ogn')
