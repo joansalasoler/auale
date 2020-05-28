c18468a4ee910853c8e0bbf76c339d5cae99ce8c
Joan Sala Soler <contact@joansala.com>
2020-05-28 21:00:05 +0200

    Base application window interface refactoring

--- src/auale/gui/__init__.py
+++ src/auale/gui/__init__.py
@@ -6,17 +6,18 @@ gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 gi.require_version('Rsvg', '2.0')
 
+from .windows import ApplicationWindow
+
 from .animator import Animator
 from .application import GTKApplication
-from .canvas import Board
 from .constants import Constants
 from .loop import GameLoop
 from .mixer import Mixer
 from .view import GTKView
 
 __all__ = [
+    'ApplicationWindow',
     'Animator',
-    'Board',
     'GameLoop',
     'Constants',
     'GTKApplication',
---
+++ src/auale/gui/windows/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .application_window import ApplicationWindow
+
+__all__ = [
+    'ApplicationWindow',
+]
