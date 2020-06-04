19c2f338c1186b7e5e22b4f40a83db2927905a49
Joan Sala Soler <contact@joansala.com>
2020-06-04 09:39:02 +0200

    Moves the services to their own module

---
+++ src/auale/gui/services/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .animator import Animator
+from .game_loop import GameLoop
+from .mixer import Mixer
+from .ponder_cache import PonderCache
+
+__all__ = [
+    'Animator',
+    'GameLoop',
+    'Mixer',
+    'PonderCache',
+]
